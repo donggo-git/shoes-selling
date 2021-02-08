317c2c549077de1efa60e2a9dbaa2f504fb51ff3
Dong <dlttthvlhhth@gmail.com>
2021-02-07 20:21:52 -0800

    add nike and puma logo

--- src/Brand.js
+++ src/Brand.js
@@ -2,7 +2,12 @@ import React from 'react';
 function Brand() {
     return (
         <div className='brand-list'>
-
+            <div className='brand-item'>
+                <img src='https://thedrop101.files.wordpress.com/2014/11/nike_swoosh_logo_black_original-1.jpg' height='50%' width='50%' />
+            </div>
+            <div className='brand-item'>
+                <img src='https://sportswearbrandstrategy.files.wordpress.com/2013/10/puma-logo-wallpaper-5298-hd-wallpapers.jpg' height='50%' width='50%' />
+            </div>
         </div>
     )
 }
