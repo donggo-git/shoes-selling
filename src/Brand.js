096db8e08bbc67c836fe53d45d8a634538448742
Dong <dlttthvlhhth@gmail.com>
2021-02-08 23:08:12 -0800

    add addidas and converse logo

--- src/Brand.js
+++ src/Brand.js
@@ -8,6 +8,12 @@ function Brand() {
             <div className='brand-item'>
                 <img src='https://sportswearbrandstrategy.files.wordpress.com/2013/10/puma-logo-wallpaper-5298-hd-wallpapers.jpg' height='50%' width='50%' />
             </div>
+            <div className='brand-item'>
+                <img src='https://th.bing.com/th/id/R257a46fbac80c94e55f0eaba653d5586?rik=67bIs9jVb0Insw&riu=http%3a%2f%2fwilliamsonsource.com%2fwp-content%2fuploads%2f2016%2f01%2fadidas-logo.png&ehk=0CbDijl8P3P3QCpglLcXoUMSpAMjNg9XTxj%2fRa6Vdps%3d&risl=&pid=ImgRaw' />
+            </div>
+            <div className='brand-item'>
+                <img src='https://heatherwoodhousebcu.files.wordpress.com/2014/10/logo-wallpapers-converse-logo-wallpaper-32444.jpg' />
+            </div>
         </div>
     )
 }
