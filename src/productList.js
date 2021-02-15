76ea2119f8e34a26fe51770a7b7d062b1b5f0757
Dong <dlttthvlhhth@gmail.com>
2021-02-14 16:57:57 -0800

    style productList

--- src/productList.js
+++ src/productList.js
@@ -8,6 +8,10 @@ function ProductList() {
             {source.map((product, index) => (
                 <div key={index} className='product-item'>
                     <img src={product.img[0]} height='100%' width='100%' />
+                    <div className='product-detail'>
+                        <h3>{product.name}</h3>
+                        <p>${product.price}</p>
+                    </div>
                 </div>
             ))}
         </div>
