926f6c1858e7ab799482992c22dba0179f48855c
Dong <dlttthvlhhth@gmail.com>
2021-02-15 23:45:12 -0800

    add brandStyle

--- src/Brand.js
+++ src/Brand.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { logo } from './source'
 import './logo-list-style.css'
-import { source } from './source'
+
 function Brand() {
-    console.log(source.length)
-    console.log(logo)
+    const [styleBrand, setStyleBrand] = useState([{ border: 'none' }, { border: 'none' }, { border: 'none' }, { border: 'none' }])
+    const handleBrandClick = (index) => {
+        let newBrandStyle = styleBrand;
+        newBrandStyle[index] = { border: '1px solid red' }
+        setStyleBrand(newBrandStyle)
+        console.log(styleBrand)
+    }
+
     return (
 
         <div className='logo-list'>
             {
                 logo.map((logoItem, index) => (
-                    < div className='brand-item' key={index}>
+                    < div className='brand-item' key={index} onClick={() => handleBrandClick(index)} style={styleBrand[index]}>
                         <img src={logoItem.img} height='50%' width='50%' />
                         {console.log(logoItem.img)}
                     </ div>
@@ -19,4 +25,5 @@ function Brand() {
         </div>
     )
 }
+
 export default Brand
--- src/Header.js
+++ src/Header.js
@@ -12,9 +12,7 @@ function Header() {
         transform: `translateX(${-styles * 80}vw)`,
         transition: transition
     }
-    let slideImageLink = ['https://images.puma.com/image/upload/q_auto,f_auto,w_1440/regional/%7Eregional%7EPNA%7Eothers%7EKOP%7EHome%7ESS21%7ERS+Curve+%7E21_SS_RS+CURVE+ADDITIONAL+IMAGES_SITE_1440x500_0000_1.jpg/fmt/jpg/fmt/png',
-        'https://www.converse.com/on/demandware.static/-/Library-Sites-SharedLibrary/default/dw99b0143b/firstspirit/media/homepage_1/2021_spring/01_1/D-Converse-SP21-1-2021-Elevation-P1-3.jpg',
-        'https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/enUS/Images/xcat-ss20-nav-clp-mens-sneakers-tc-large-d_tcm221-509820.jpg']
+
     const nextSlice = () => {
         setStyles(styles + 1)
         setTransition('all 0.6s')
