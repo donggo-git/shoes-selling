cf50e9da7022e786e89959b8086a4c24952cad86
Dong <dlttthvlhhth@gmail.com>
2021-02-17 23:16:05 -0800

    change styleBrand to useState

--- src/Brand.js
+++ src/Brand.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { logo } from './source'
 import './logo-list-style.css'
 
-class Brand extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            styleBrand: [{ border: 'none' }, { border: 'none' }, { border: 'none' }, { border: 'none' }]
-        }
+function Brand() {
+    const [styleBrand, setStyleBrand] = useState([{ border: '1px solid #333' }, { border: 'none' }, { border: 'none' }, { border: 'none' }])
+
+    const handleBrandClick = (index) => {
+        let newBrandStyle = [{ border: 'none' }, { border: 'none' }, { border: 'none' }, { border: 'none' }];
+        newBrandStyle[index] = { border: '1px solid #333' }
+        setStyleBrand(newBrandStyle)
+
     }
-    render() {
-        const handleBrandClick = (index) => {
-            let newBrandStyle = [{ border: 'none' }, { border: 'none' }, { border: 'none' }, { border: 'none' }];
-            newBrandStyle[index] = { border: '1px solid #333' }
-            this.setState({
-                styleBrand: newBrandStyle
-            })
 
-        }
+    return (
 
-        return (
+        <div className='logo-list'>
+            {
+                logo.map((logoItem, index) => (
+                    < div className='brand-item' key={index} style={styleBrand[index]} onClick={() => handleBrandClick(index)}>
+                        <img src={logoItem.img} height='50%' width='50%' />
+                        {console.log(logoItem.img)}
+                    </ div>
+                ))
+            }
+        </div>
+    )
 
-            <div className='logo-list'>
-                {
-                    logo.map((logoItem, index) => (
-                        < div className='brand-item' key={index} style={this.state.styleBrand[index]} onClick={() => handleBrandClick(index)}>
-                            <img src={logoItem.img} height='50%' width='50%' />
-                            {console.log(logoItem.img)}
-                        </ div>
-                    ))
-                }
-            </div>
-        )
-    }
 }
 export default Brand
