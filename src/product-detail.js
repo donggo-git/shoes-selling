ac96fe635f27417314c1a144d90ac9e90aa76cad
Dong <dlttthvlhhth@gmail.com>
2021-02-21 22:37:47 -0800

    create product-detail file

---
+++ src/product-detail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+function Product_detail(props) {
+    return (
+        <div>
+            <AiOutlineClose />
+            <div></div>
+        </div>
+    )
+}
+export default Product_detail
