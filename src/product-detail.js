40880a0c131e9bfeef07460176c7f76b75bba932
Dong <dlttthvlhhth@gmail.com>
2021-02-22 22:21:10 -0800

    update product-detail

--- src/product-detail.js
+++ src/product-detail.js
@@ -4,7 +4,15 @@ function Product_detail(props) {
     return (
         <div>
             <AiOutlineClose />
-            <div></div>
+            <div>
+                <img src={props.img} />
+                <div>
+                    <h2>{props.name}</h2>
+                    <p>{props.brand}</p>
+                    <p>{props.price}</p>
+                    <p>{props.detail}</p>
+                </div>
+            </div>
         </div>
     )
 }
