230b735cb73f32a44add674a3a1fa9013a39e395
Dong <dlttthvlhhth@gmail.com>
2021-02-24 09:23:30 -0800

    update style to product-detail

--- src/product-detail.js
+++ src/product-detail.js
@@ -3,11 +3,12 @@ import { AiOutlineClose } from 'react-icons/ai'
 import './detailPage.css'
 function Product_detail(props) {
     return (
-        <div className='detail-container'>
-            <AiOutlineClose />
-            <div className='detail'>
+        <div className='detail-container' style={{ background: props.product.color[0] }}>
+
+            <div className='detail' >
                 <img src={props.product.img[0]} />
-                <div>
+                <div className='detail-content'>
+                    <AiOutlineClose className='close-btn' />
                     <h2>{props.product.name}</h2>
                     <p>{props.product.brand}</p>
                     <p>{props.product.price}</p>
