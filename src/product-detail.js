85c06a0c502c866f89e0bb02fb4a59c9351e3de4
Dong <dlttthvlhhth@gmail.com>
2021-02-24 09:56:54 -0800

    add color-list to product-detail

--- src/product-detail.js
+++ src/product-detail.js
@@ -3,16 +3,26 @@ import { AiOutlineClose } from 'react-icons/ai'
 import './detailPage.css'
 function Product_detail(props) {
     return (
-        <div className='detail-container' style={{ background: props.product.color[0] }}>
+        <div className='modal'>
+            <div className='detail-container' style={{ background: props.product.color[0] }}>
 
-            <div className='detail' >
-                <img src={props.product.img[0]} />
-                <div className='detail-content'>
-                    <AiOutlineClose className='close-btn' />
-                    <h2>{props.product.name}</h2>
-                    <p>{props.product.brand}</p>
-                    <p>{props.product.price}</p>
-                    <p>{props.product.detail}</p>
+                <div className='detail' >
+                    <div className='detail-img'>
+                        <img src={props.product.img[0]} />
+                        <div className='color-list'>
+                            {props.product.color.map((color) =>
+                                <div style={{ background: color }} className='color-item' />
+
+                            )}
+                        </div>
+                    </div>
+                    <div className='detail-content'>
+                        <AiOutlineClose className='close-btn' />
+                        <h2>{props.product.name}</h2>
+                        <p>{props.product.brand}</p>
+                        <p>{props.product.price}</p>
+                        <p>{props.product.detail}</p>
+                    </div>
                 </div>
             </div>
         </div>
