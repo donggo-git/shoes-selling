97075a5e88a885593d647c7500e26df8cc6a7f1a
Dong <Dlttthvlhhth@gmail.com>
2021-03-24 22:44:03 -0700

    add discountStyle

--- src/Summary.js
+++ src/Summary.js
@@ -3,24 +3,30 @@ import './Summary.css'
 function Summary(props) {
     let tax = Math.round(props.subtotal * 10.1) / 100;
     let [total, setTotal] = useState(Math.round((props.subtotal + tax + 8) * 100) / 100);
-
     let [codeEnter, setCodeEnter] = useState('');
     let [discountAmount, setDiscountAmount] = useState(0)
+    let [discountStyle, setDiscountStyle] = useState({ display: 'none' })
     const handleEnterCode = (e) => {
         setCodeEnter(e);
     }
     const handleSubmitCode = () => {
         switch (codeEnter) {
             case '':
-                return
+                setDiscountStyle({ display: 'none' })
                 break;
             case 'discount10':
                 setDiscountAmount(Math.round(total * 10) / 100);
+                setDiscountStyle({ display: 'flex' })
                 setTotal(total - discountAmount)
                 break;
             case 'discount20':
                 setDiscountAmount(Math.round(total * 20) / 100);
+                setDiscountStyle({ display: 'flex' })
                 setTotal(total - discountAmount);
+                break;
+            default:
+                alert('You enter the wrong code')
+                setDiscountStyle({ display: 'none' })
                 break;
         }
         setCodeEnter('')
@@ -51,7 +57,8 @@ function Summary(props) {
                 <p>Estimated tax</p>
                 <p>{tax}</p>
             </div>
-            <div className='summary__textLine summary__textLine__discount'>
+            <div className='summary__textLine summary__textLine__discount' style={discountStyle}>
+                <p>Discount</p>
                 <p>-{discountAmount}</p>
             </div>
             <div className='summary__textLine total'>
