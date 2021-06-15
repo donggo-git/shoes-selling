e1a01d1f17b503accb44fa4e6a0a66fdaedffe79
Dong <Dlttthvlhhth@gmail.com>
2021-06-14 23:04:46 -0700

    add style and translate  for trendingProducts

--- src/TrendingProducts.js
+++ src/TrendingProducts.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TrendingProducts.css"
 import { AiOutlineRight } from 'react-icons/ai'
 import { AiOutlineLeft } from 'react-icons/ai'
 
 function TrendingProducts({ products }) {
-    console.log(products);
-    const handleScrollTrending = () => { }
+    const [translateSlide, setTranslateSlide] = useState({
+        transform: "translateX(0)"
+    })
+    const handleScrollTrending = () => {
+        setTranslateSlide({
+            transform: "translateX(-50px)"
+        })
+        console.log("Hello")
+    }
     return (
         <div className="Trending_container">
             <h2 className="title">Trending</h2>
             <div className="Trending_btn previousBtn_container"><AiOutlineLeft /></div>
-            <div className="Trending_btn nextBtn_container"><AiOutlineRight /></div>
+            <div
+                className="Trending_btn nextBtn_container"
+
+                onClick={() => handleScrollTrending()}>
+                <AiOutlineRight />
+            </div>
             <div className='Trending_slide_container'>
 
-                <div className="Trending_slide">
+                <div className="Trending_slide"
+                    style={translateSlide}>
                     {
                         products
                             .filter(product => (product.product.trending == true))
