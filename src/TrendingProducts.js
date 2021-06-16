8c6ae58bacc50b4d5ca3008590345b61d5093bc4
Dong <Dlttthvlhhth@gmail.com>
2021-06-15 22:02:36 -0700

    finish the handleScrollTrending

--- src/TrendingProducts.js
+++ src/TrendingProducts.js
@@ -4,29 +4,38 @@ import { AiOutlineRight } from 'react-icons/ai'
 import { AiOutlineLeft } from 'react-icons/ai'
 
 function TrendingProducts({ products }) {
-    const [translateSlide, setTranslateSlide] = useState({
-        transform: "translateX(0)"
-    })
-    const handleScrollTrending = () => {
-        setTranslateSlide({
-            transform: "translateX(-50px)"
-        })
-        console.log("Hello")
+    const [translateProgress, setTranslateProgress] = useState(0)
+    let styledTrending = {
+        transform: `translateX(${translateProgress}rem)`
+    }
+    const handleScrollTrending = (button) => {
+        if (button == "next") {
+            if (translateProgress > -32) {
+                setTranslateProgress((pre) => pre -= 16)
+            }
+        }
+        else {
+            if (translateProgress)
+                setTranslateProgress((pre) => pre += 16)
+        }
     }
     return (
         <div className="Trending_container">
             <h2 className="title">Trending</h2>
-            <div className="Trending_btn previousBtn_container"><AiOutlineLeft /></div>
+            <div
+                className="Trending_btn previousBtn_container"
+                onClick={() => handleScrollTrending("previous")}
+            ><AiOutlineLeft /></div>
             <div
                 className="Trending_btn nextBtn_container"
 
-                onClick={() => handleScrollTrending()}>
+                onClick={() => handleScrollTrending("next")}>
                 <AiOutlineRight />
             </div>
             <div className='Trending_slide_container'>
 
                 <div className="Trending_slide"
-                    style={translateSlide}>
+                    style={styledTrending}>
                     {
                         products
                             .filter(product => (product.product.trending == true))
@@ -34,7 +43,7 @@ function TrendingProducts({ products }) {
                                 <div key={product.id} className="Trending_slide_product">
                                     <img src={product.product.img[0]} height='100%' width='100%' />
                                     <p>{product.product.name}</p>
-                                    <p>{product.product.price}</p>
+                                    <p>$ {product.product.price}</p>
                                 </div>
                             ))
                     }
