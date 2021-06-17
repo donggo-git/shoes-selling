983fb81d869cd28676d4dde375fb5414060c117a
Dong <Dlttthvlhhth@gmail.com>
2021-06-16 23:53:29 -0700

    add brand in filterForm

---
+++ src/FilterForm.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './FilterProduct.css'
+
+function FilterForm(props) {
+    let categories = ['Soccer', 'Training', 'Basketball', 'Running', 'Football']
+    const handleFilter = (e) => {
+
+    }
+    return (
+        <div className='filter'>
+            <div className='filter__line'>
+                <p>Gender</p>
+                <div>
+                    <input type='checkbox' value='Men' name='Gender' onChange={(e) => props.filterByGender(e)} />
+                    <label>Men</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='Women' name='Gender' onChange={(e) => props.filterByGender(e)} />
+                    <label>Women</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='Unisex' name='Gender' onChange={(e) => props.filterByGender(e)} />
+                    <label>Unisex</label>
+                </div>
+            </div>
+            <div className='filter__line'>
+                <p>Brand</p>
+                <div>
+                    <input type='checkbox' value='Nike' name='Brand' onChange={(e) => props.filterByGender(e)} />
+                    <label>Nike</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='Puma' name='Brand' onChange={(e) => props.filterByGender(e)} />
+                    <label>Puma</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='Adidas' name='Brand' onChange={(e) => props.filterByGender(e)} />
+                    <label>Adidas</label>
+                </div>
+                <div>
+                    <input type='checkbox' value='Rebook' name='Brand' onChange={(e) => props.filterByGender(e)} />
+                    <label>Rebook</label>
+                </div>
+            </div>
+            <div className='filter__line'>
+                <p>Shop by Price</p>
+                <div>
+                    <input type='checkbox' value={[0, 25]} name='Price' onChange={(e) => props.TrackFilterForm(e)} />
+                    <label for='0-25'>$0-$25</label>
+                </div>
+                <div>
+                    <input type='checkbox' value={[25, 50]} name='Price' onChange={(e) => props.TrackFilterForm(e)} />
+                    <label for='25-50'>$25-$50</label>
+                </div>
+                <div>
+                    <input type='checkbox' value={[50, 100]} name='Price' onChange={(e) => props.TrackFilterForm(e)} />
+                    <label for='50-100'>$50-$100</label>
+                </div>
+                <div>
+                    <input type='checkbox' value={[100, 150]} name='Price' onChange={(e) => props.TrackFilterForm(e)} />
+                    <label for='100-150'>$100-$150</label>
+                </div>
+                <div>
+                    <input type='checkbox' value={[150, 10000]} name='Price' onChange={(e) => props.TrackFilterForm(e)} />
+                    <label for='150-10000'>Over $150</label>
+                </div>
+            </div>
+            <div className='filter__line category'>
+                {categories.map((category) => (
+                    <div className='category__label'>
+                        <input type='radio' value={category} name='categories' onChange={(e) => props.TrackFilterForm(e)} />
+                        <label>{category}</label>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default FilterForm
--- src/productList.js
+++ src/productList.js
@@ -5,19 +5,45 @@ import { source } from './source'
 import './productList.css'
 import './detailPage.css'
 import Product_detail from './product-detail'
-import FilteForm from './FilterProuct'
-function ProductList(props) {
+import FilterForm from './FilterForm'
+function ProductList({ products }) {
 
     // filter product by the filter form 
+
+    // list of product after filter
+    const [filterProduct, setFilterProduct] = useState([...products])
+    const [filterCheckBox, setFilterCheckBox] = useState({
+        Gender: {
+            Men: false,
+            Women: false,
+            Unisex: false
+        },
+        Price: [0, 10000],
+        Brand: {
+            Nike: false,
+            Puma: false,
+            Adidas: false,
+            Rebook: false
+        }
+    })
+    const filterHandle = (filterEvent) => {
+        if (filterEvent.name == "Gender") {
+
+        }
+    }
+    const filterByGender = (gender) => {
+
+    }
+
     return (
         <div>
             <h2 className="title">Product</h2>
             <div className='product-list-container'>
-                <FilteForm />
+                <FilterForm filterByGender={filterByGender} />
                 <div className='product-list-show'>
 
                     <div className='product-list'>
-                        {props.products.map((product) => (
+                        {products.map((product) => (
                             <div key={product.product.id} className='product-item' >
                                 <img src={product.product.img[0]} height='100%' width='100%' alt={product.name + ' shoes'} />
                                 <div className='product-detail'>
