39e1752fb839532d3900fe3403ab9cc1d7c3204d
Dong <Dlttthvlhhth@gmail.com>
2021-07-20 23:14:53 -0700

    add style to checkbox

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Checkbox from '@material-ui/core/Checkbox';
 import "./FilterForm.css"
 function FilterForm(props) {
     const [categories, setCategories] = useState({
@@ -11,6 +12,14 @@ function FilterForm(props) {
     const CategoryStyle = {
         color: 'rgb(114, 114, 114)'
     }
+    const styles = () => ({
+        root: {
+            "&$checked": {
+                color: "rgba(0, 0, 0, 0.54)"
+            }
+        },
+        checked: {}
+    })
     const handleCategories = (e) => {
         let newCategories = { ...categories }
         let passCategory = {
@@ -42,57 +51,106 @@ function FilterForm(props) {
             <div className='filter__line'>
                 <p>Gender</p>
                 <div>
-                    <input type='checkbox' value='Men' name='gender' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Men'
+                        name='gender' className="checkbox"
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Men</label>
                 </div>
                 <div>
-                    <input type='checkbox' value='Women' name='gender' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Women'
+                        name='gender' className="checkbox"
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Women</label>
                 </div>
                 <div>
-                    <input type='checkbox' value='Unisex' name='gender' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Unisex'
+                        name='gender'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Unisex</label>
                 </div>
             </div>
             <div className='filter__line'>
                 <p>Brand</p>
                 <div>
-                    <input type='checkbox' value='Nike' name='brand' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Nike'
+                        name='brand'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Nike</label>
                 </div>
                 <div>
-                    <input type='checkbox' value='Puma' name='brand' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Puma'
+                        name='brand'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Puma</label>
                 </div>
                 <div>
-                    <input type='checkbox' value='Adidas' name='brand' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Adidas'
+                        name='brand'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Adidas</label>
                 </div>
                 <div>
-                    <input type='checkbox' value='Rebook' name='brand' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value='Rebook'
+                        name='brand'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label>Rebook</label>
                 </div>
             </div>
             <div className='filter__line'>
                 <p>Shop by Price</p>
                 <div>
-                    <input type='checkbox' value={[0, 25]} name='price' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value={[0, 25]}
+                        name='price'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label for='0-25'>$0-$25</label>
                 </div>
                 <div>
-                    <input type='checkbox' value={[25, 50]} name='price' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value={[25, 50]}
+                        name='price'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label for='25-50'>$25-$50</label>
                 </div>
                 <div>
-                    <input type='checkbox' value={[50, 100]} name='price' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value={[50, 100]}
+                        name='price'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label for='50-100'>$50-$100</label>
                 </div>
                 <div>
-                    <input type='checkbox' value={[100, 150]} name='price' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox' value={[100, 150]}
+                        name='price'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label for='100-150'>$100-$150</label>
                 </div>
                 <div>
-                    <input type='checkbox' value={[150, 10000]} name='price' onChange={(e) => props.filterHandle(e)} />
+                    <Checkbox
+                        size="small"
+                        type='checkbox'
+                        value={[150, 10000]}
+                        name='price'
+                        onChange={(e) => props.filterHandle(e)} />
                     <label for='150-10000'>Over $150</label>
                 </div>
             </div>
