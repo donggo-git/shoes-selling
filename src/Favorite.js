6a6dcb422d5a4a160dcac896ae96966575b7de24
Dong <Dlttthvlhhth@gmail.com>
2021-07-31 23:12:31 -0700

    add style favorite page

--- src/Favorite.js
+++ src/Favorite.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import "./Favorite.css"
 
 function Favorite({ favorite }) {
     return (
-        <div>
+        <div className='favorite_page'>
             {favorite.map(item => (
-                <div>
+                <div className='favorite_item'>
                     <div className="favorite_img_container">
-                        <img src={item.product.img[0]} height="100%" />
+                        <img src={item.product.img[0]} height="100%" width="100%" />
                     </div>
                     <div className="favorite_item_content">
                         <h2>{item.product.name}</h2>
-                        <p>{item.product.description}</p>
+                        <p>{item.product.brand}</p>
+                        <p>{item.product.price}</p>
                     </div>
                 </div>
             ))}
